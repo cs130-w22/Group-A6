b67c9580984b051e27725b211112b8e11e6c1027
jimzhou112 <jimzhou.112p@gmail.com>
2022-02-10 21:44:42 -0800

    Added main, about, error pages Changes to be committed: 	modified:   index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
 const port = 3000
 
+app.use(express.static('../frontend/letsMeet/Assets.xcassets'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('User is in the home page')
+  res.status(200).sendFile(path.join(__dirname, "../frontend/index.html")); //placeholder
+})
+
+app.get('/about', (req, res) => {
+  console.log('User is in the about page');
+  res.status(200).send('About page');
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(port, () => {
