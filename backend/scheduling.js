7b8091bede68b03e62b527052abc42d711c3e865
jimzhou112 <jimzhou.112p@gmail.com>
2022-02-20 00:42:43 -0800

    Added support for variable number of optimal meeting times - Extended module from single optimal meeting time to multiple - Default number of optimal meetings is 5 - Modified testbench to support new return type - Returns null arrays if not enough optimal meeting times can be found

--- backend/scheduling.js
+++ backend/scheduling.js
@@ -1,39 +1,21 @@
 /**
- * This function returns one optimal meeting time.
+ * This function returns up to a variable number (default = 5) of optimal meeting times.
  *
  * @param {array of array of time intervals} conflictTimes: Each element is an array which represents one meeting participant's times which conflict with other events.
  * @param {array of array of time intervals} preferredTimes: Each element is an array which represents one meeting participant's preferred meeting times.
  * @param {Date object} startTime: Earliest time that the meeting can be held.
  * @param {Date object} endTime: Latest time that the meeting can be held.
  * @param {integer} meetingDuration: Length of the meeting (minutes).
- * @return {array of time interval} Uses UTC time.
+ * @return {array of time intervals} Uses UTC time.
  * 
  * Note: a time interval is a 2-element array of JS Date objects
- * The function priotizes a meeting time which has the least amount of conflictTimes, followed by the most preferredTimes among the participants. 
- * Pseudocode:
-    num_conflict = inf
-    num_prefer = inf
-    curr_conflict = 0
-    curr_prefer = 0
-
-    iterate through each 5 min interval from startTime to (endTime - meetingDuration):
-        set meeting to start then and end after meetingDuration
-        iterate over each conflict:
-            if it intersects with meeting:
-                curr_conflict += 1
-        iterate over each prefer:
-            if it intersects with meeting:
-                curr_prefer += 1
-        if curr_conflict <  num_conflict or (curr_conflict == num_conflict and curr_prefer > num_prefer):
-            num_conflict = curr_conflict
-            num_prefer = curr_prefer
-            set current meeting as the best
-        curr_conflict = 0
-        curr_prefer = 0
-    return best meeting time
+ * The function priotizes a meeting time which has the least amount of conflictTimes, followed by the most preferredTimes among the participants. If it is not possible to find the requested number of meeting times, the remaining time intervals in the array will be null arrays.
+ * 
  */
 
 function calculateOptimalMeetingTime(conflictTimes, preferredTimes, startTime, endTime, meetingDuration) {
+    const NUM_TIMES = 5; // Number of times to return
+
     let num_conflict = Number.MAX_SAFE_INTEGER;
     let num_prefer = Number.MAX_SAFE_INTEGER;
 
@@ -43,20 +25,17 @@ function calculateOptimalMeetingTime(conflictTimes, preferredTimes, startTime, e
     let curr_conflict = 0;
     let curr_prefer = 0;
 
+    let return_array = [];
 
     meeting_start = startTime;
     meeting_end = new Date(meeting_start);
-    // console.log(meeting_end)
-    // console.log(meetingDuration)
-    // console.log(startTime)
     meeting_end.setMinutes(startTime.getMinutes() + meetingDuration);
-    // console.log(meeting_start);
-    // console.log(meeting_end);
-    const INTERVAL = 5; // We search over each 5 minute period by default 
+
+    const INTERVAL = 30; // We search over each 30 minute period by default 
     while (meeting_end <= endTime){
         for (const participant of conflictTimes) { // Loop over each participant
             for (const time_interval of participant) { // Loop over each time interval
-                if ((time_interval[0] > meeting_start ? time_interval[0] : meeting_start) <= (time_interval[1] < meeting_end ? time_interval[0] : meeting_end)){
+                if ((time_interval[0] > meeting_start ? time_interval[0] : meeting_start) <= (time_interval[1] < meeting_end ? time_interval[1] : meeting_end)){
                     curr_conflict += 1;
                     break;
                 }
@@ -71,34 +50,32 @@ function calculateOptimalMeetingTime(conflictTimes, preferredTimes, startTime, e
                 }
             }
         }
-        // console.log("TEST")
-        // console.log(curr_conflict)
-        // console.log(num_conflict)
-        // console.log(curr_prefer);
-        // console.log(num_prefer);
-        if (curr_conflict <  num_conflict || (curr_conflict == num_conflict && curr_prefer > num_prefer)){
-            console.log("TEST")
-            console.log(curr_conflict)
-            console.log(curr_prefer)
-            console.log(meeting_start)
-            console.log(meeting_end)
-            num_conflict = curr_conflict
-            num_prefer = curr_prefer
-            best_start = new Date(meeting_start);
-            best_end = new Date(meeting_end);
+
+        if (curr_conflict < num_conflict || (curr_conflict == num_conflict && curr_prefer > num_prefer)){
+            num_conflict = curr_conflict;
+            num_prefer = curr_prefer;
+            return_array = [[new Date(meeting_start), new Date(meeting_end)]];
         }
-        // console.log("TEST")
-        // console.log(curr_conflict)
-        // console.log(curr_prefer)
-        // console.log(meeting_start)
-        // console.log(meeting_end)
-        curr_conflict = 0
-        curr_prefer = 0
+        else if (curr_conflict == num_conflict && curr_prefer == num_prefer){
+            return_array.push([new Date(meeting_start), new Date(meeting_end)])
+        }
+
+        curr_conflict = 0;
+        curr_prefer = 0;
 
         meeting_start.setMinutes(meeting_start.getMinutes() + INTERVAL);
         meeting_end.setMinutes(meeting_end.getMinutes() + INTERVAL);
     } 
-    return [best_start, best_end];
+    
+    if (return_array.length > NUM_TIMES){
+        return return_array.slice(0, NUM_TIMES);
+    }
+    else{
+        while (return_array.length < NUM_TIMES){
+            return_array.push([null, null])
+        }
+        return return_array;
+    }
 
 }
 
